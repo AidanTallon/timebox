d0a3125d8f5aad8486e38687f5083613cc660c5b
Aidan Tallon <atallon@spartaglobal.co>
2016-11-05 16:40:57 +0000

    Refactor code. Change test mode to run at 20x speed

--- main.py
+++ main.py
@@ -17,6 +17,9 @@ songs = ["Danger! Danger! High Voltage! When we touch. When we kiss.",
 	"I want to know what love is. I want you to show me.",
 	"I'm bringing sexy back."]
 
+## When to alert user of time left. Only alerts that are less than half of the timebox length are used
+alerts = [60, 300, 600, 900, 1800]
+
 ## Parse args
 parser = argparse.ArgumentParser('Set a timebox.')
 parser.add_argument('minutes', help="Number of minutes to timebox.", type=int)
@@ -24,34 +27,64 @@ parser.add_argument('-s', help='Silent timebox.', action="store_true")
 parser.add_argument('--song', help='Sing a song when finished.', action="store_true")
 parser.add_argument('-a', help="Alerts throughout timebox.", action="store_true")
 parser.add_argument('-r', help="Timebox alert is a randomized phrase.", action="store_true")
-parser.add_argument('--test', help="Run 10 second timebox for testing purposes.", action="store_true")
+parser.add_argument('--test', help="Test mode reduces sleep time", action="store_true")
 args = parser.parse_args()
 
-## Time
-minutes = int(args.minutes)
+## Main program loop
+def main():
+	## Check if test mode
+	count = int(args.minutes) * 60
+	if args.test:
+		countdown(count, test=True)
+	else:
+		countdown(count)
+	notification_end()
 
-count = minutes * 60
+## Countdown loop
+def countdown(c, test=False):
+	## used for testing purposes
+	if test is True:
+		n = 0.05
+	else:
+		n = 1
 
-## Test Mode
-if args.test:
-	count = 10
+	while c >= 0:
+		write(c)
+		c -= 1
+		## halfway mark
+		if (c == args.minutes * 30) or (c in alerts and c < args.minutes * 30):
+			## needs to be async
+			notification_duration(c)
+		time.sleep(n)
 
-## Countdown
-while count >= 0:
+## Writes time remaining to terminal
+def write(c):
+	m = int(c / 60)
+	s = c % 60
 	sys.stdout.flush()
-	minute = int(count / 60)
-	second = count % 60
-	sys.stdout.write("{:0>2d}:{:0>2d}\r".format(minute, second))
-	count -= 1
-	time.sleep(1)
+	sys.stdout.write("{:0>2d}:{:0>2d}\r".format(m, s))
 
-## End Result
-print "Timebox finished"
-if args.s:
-	pass
-elif args.song:
-	os.system('say "{0}"'.format(songs[randint(0, len(songs) - 1)]))
-elif args.r:
-	os.system('say "{0}"'.format(phrases[randint(0, len(phrases) - 1)]))
-else:
-	os.system('say {0}'.format(phrases[0]))
+## Notification that timebox finished
+def notification_end():
+	print "Timebox finished"
+	if args.s:
+		pass
+	elif args.song:
+		os.system('say "{0}"'.format(songs[randint(0, len(songs) - 1)]))
+	elif args.r:
+		os.system('say "{0}"'.format(phrases[randint(0, len(phrases) - 1)]))
+	else:
+		os.system('say "{0}"'.format(phrases[0]))
+
+## Notification of time remaining
+def notification_duration(c):
+	if args.a is True and args.s is False:
+		m = int(c / 60)
+		if m == 1:
+			msg = "1 minute remaining."
+		else:
+			msg = "{0} minutes remaining.".format(m)
+		os.system('say "{0}"'.format(msg))
+
+
+main()
